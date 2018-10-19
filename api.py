eeaf58fdaf0cebf60b18d4f86bd79ba335882875
Ian Morgan <ianmo@zillowgroup.com>
2018-10-19 14:36:56 -0700

    show time elapsed in ms, closes #3

--- api.py
+++ api.py
@@ -14,12 +14,15 @@ def domain_response_html(req, resp, *, protocol, domain):
 
     domain_response_code = r.status_code
     domain_response_text = r.text
+    domain_response_time_ms = r.elapsed.microseconds / 1000
+
     resp.content = api.template(
             'ping_response.html',
             domain=domain,
-            domain_response_code=domain_response_code, 
-            domain_response_text=domain_response_text
+            domain_response_code=domain_response_code,
+            domain_response_text=domain_response_text,
+            domain_response_time_ms=domain_response_time_ms
     )
 
 if __name__ == '__main__':
-    api.run(address="0.0.0.0", port=80)
+    api.run(address="0.0.0.0", port=8080)
